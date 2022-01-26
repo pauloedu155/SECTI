241951534548d9a2f21a76a2fb0c09e521ac1748
pauloedu155 <98470869+pauloedu155@users.noreply.github.com>
2022-01-26 11:24:17 -0400

    feat(new version): Add new version

---
+++ myapp/lib/src/pages/empty_1/page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import 'package:myapp/auth/auth_state.dart';
+
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:myapp/src/pages/index.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:map/map.dart';
+import 'package:latlng/latlng.dart';
+import 'package:paged_vertical_calendar/paged_vertical_calendar.dart';
+
+class PageEmptyA1 extends StatefulWidget {
+  const PageEmptyA1({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<PageEmptyA1> {
+  WebViewXController? state1 = 0;
+
+  final datasets = <String, dynamic>{};
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF000000),
+      body: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.zero,
+            padding: EdgeInsets.zero,
+            width: double.maxFinite,
+            decoration: const BoxDecoration(
+              color: Color(0xFF000000),
+            ),
+            child: Align(
+              alignment: null,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
